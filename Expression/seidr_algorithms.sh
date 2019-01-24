7e431ae0752f5f7d30737b9c5b621e358a0fcf1f
Hui Liu <liuhui.bfu@gmail.com>
2019-01-25 06:51:39 +0800

    Update and rename seidr.sh to seidr_algorithms.sh

--- Expression/seidr_algorithms.sh
+++ Expression/seidr_algorithms.sh
@@ -1,37 +1,36 @@
 # https://www.biorxiv.org/content/biorxiv/early/2018/01/19/250696.full.pdf
-# (1) ARACNE (at jolvii server)
+# (1) ARACNE
 mi -m ARACNE --force -i data/xso_expr_mat.txt -g data/xso_genes.txt -o seidr_results/xso_edgelist_ARACNE.tsv
 
-# (2) CLR (at pine)
+# (2) CLR
 mpirun -np 9 --oversubscribe --allow-run-as-root mi -m CLR --force -i data/xso_expr_mat.txt -g data/xso_genes.txt -o seidr_results/xso_edgelist_CLR.tsv
-seidr import --force -u -z -r -i seidr_results/xso_edgelist_CLR.tsv -g data/xso_genes.txt -F lm -o seidr_results/xso_CLR_ranks.sf
 
-# (3) pearson (at jolvii server)
+# (3) pearson
 correlation --force -m pearson -i data/xso_expr_mat.txt -g data/xso_genes.txt -o seidr_results/xso_edgelist_pearson.tsv
 
-# (4) spearman (at jolvii server)
+# (4) spearman
 correlation --force -m spearman -i data/xso_expr_mat.txt -g data/xso_genes.txt -o seidr_results/xso_edgelist_spearman.tsv
 
-# (5) ensemble of Elastic Net regression predictors (at jolvii server)
-mpirun -np 9 --oversubscribe --allow-run-as-root el-ensemble --force -i data/xso_expr_mat.txt -g data/xso_genes.txt -o seidr_results/xso_edgelist_el.tsv
+# (5) ensemble of Elastic Net regression predictors
+mpirun -np 9 --oversubscribe --allow-run-as-root el-ensemble --force -i data/xso_expr_mat.txt -g data/xso_genes.txt -o seidr_results/xso_edgelist_elnet.tsv
 
-# (6) ensemble of Support Vector Machine predictor (at crick server)
+# (6) ensemble of Support Vector Machine predictor
 mpirun -np 16 --oversubscribe --allow-run-as-root svm-ensemble --force -i data/xso_expr_mat.txt -g data/xso_genes.txt -o seidr_results/xso_edgelist_svm.tsv
 
-# (7) ensemble of Support Vector Machine predictors (at crick server)
+# (7) ensemble of Support Vector Machine predictors
 mpirun -np 16 --oversubscribe --allow-run-as-root llr-ensemble --force -i data/xso_expr_mat.txt -g data/xso_genes.txt -o seidr_results/xso_edgelist_llr.tsv
 
-# (8) GENIE3 (at jolvii server)
+# (8) GENIE3
 genie3 --force -i data/xso_expr_mat.txt -g data/xso_genes.txt -o seidr_results/xso_edgelist_genie3.tsv
 
-# (9) Narromi (at pine)
+# (9) Narromi
 mpirun -np 9 --oversubscribe --allow-run-as-root narromi --force -i data/xso_expr_mat.txt -g data/xso_genes.txt -o seidr_results/xso_edgelist_narromi.tsv
 
-# (10) Partial Correlation (at jolvii server)
+# (10) Partial Correlation
 pcor --force -i data/xso_expr_mat.txt -g data/xso_genes.txt -o seidr_results/xso_edgelist_pcor.tsv
 
-# (11) PLSNET (at pine)
+# (11) PLSNET
 mpirun -np 9 --oversubscribe --allow-run-as-root plsnet --force -i data/xso_expr_mat.txt -g data/xso_genes.txt -o seidr_results/xso_edgelist_plsnet.tsv
 
-# (12) TIGRESS (at pine)
+# (12) TIGRESS
 mpirun -np 9 --oversubscribe --allow-run-as-root tigress --force -i data/xso_expr_mat.txt -g data/xso_genes.txt -o seidr_results/xso_edgelist_tigress.tsv
