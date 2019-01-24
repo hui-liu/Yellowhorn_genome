404617318155e8c85f7ccfb7b7323884c026ebb8
Hui Liu <liuhui.bfu@gmail.com>
2019-01-24 18:59:30 +0800

    Update xso_gene_expr.sh

--- Expression/xso_gene_expr.sh
+++ Expression/xso_gene_expr.sh
@@ -1,21 +1,24 @@
+###############
+# build index
+###############
+cd /Pinus1/Liuhui/Xsorbifolium/gene_expression/index
 RefGene=final.protein_gene.gtf
 RefGenome=genome2.fasta
 
-# build index
-cd /Pinus1/Liuhui/Xsorbifolium/gene_expression/index
 extract_splice_sites.py $RefGene > $RefGene.ss
 extract_exons.py $RefGene > $RefGene.exon
 hisat2-build --ss $RefGene.ss --exon $RefGene.exon $RefGenome $RefGenome
 
 
+###############
 # mapping and estimate the abundance
+###############
 cd /Pinus1/Liuhui/Xsorbifolium/gene_expression
 Index=/Pinus1/Liuhui/Xsorbifolium/gene_expression/index/genome2.fasta
 RefGene=/Pinus1/Liuhui/Xsorbifolium/gene_expression/index/final.protein_gene.gtf
 
 htoutdir=ht_out && mkdir -p $htoutdir
 stoutdir=st_out && mkdir -p $stoutdir
-
 ncpu=10
 ht_opts="-k 1 --new-summary"
 
@@ -29,24 +32,9 @@ do
 	outdir=$stoutdir/${SAMPLE}
 	mkdir -p $outdir
 	stringtie -e -B -p $ncpu -G $RefGene -A $outdir/gene_exp.tab -o $outdir/transcript.gtf $htoutdir/${SAMPLE}.bam   
-    featureCounts --donotsort -p -Q 10 -t exon -g gene_id -s 0 -T 10 -a $RefGene -o $outdir/gene_exp.cnt $htoutdir/${SAMPLE}.bam
+    #featureCounts --donotsort -p -Q 10 -t exon -g gene_id -s 0 -T 10 -a $RefGene -o $outdir/gene_exp.cnt $htoutdir/${SAMPLE}.bam
 done
-
-# for SAMPLE in `cat sample_list`
-# do
-    # outdir=$stoutdir/${SAMPLE}
-    # echo "htseq-count -f bam -r name -s no -a 10 -t exon -i gene_id -m union --nonunique=none $htoutdir/${SAMPLE}.bam $RefGene > $outdir/${SAMPLE}.count"
-# done
-
 cat sample_list | xargs -n 1 -P 10 ./htseq.sh
-
-# mapping rate
-cd /Pinus1/Liuhui/Xsorbifolium/gene_expression/ht_out
-grep -E "concordantly 1 time|Overall" *summary | \
-awk '{print $1,$NF}' | sed 's/(//;s/)//;s/.summary: /\t/' | \
-awk '{a[$1]=($1 in a ? a[$1]"\t"$2: $0)} END{for (k in a) print a[k]}' | \
-sed '1isamples\tunique_mappingRate\tmapping_rate' > ../Xso_RNAseq_MappingRate.xls
-
 
 # htseq-count results
 cp */*count ../htseq_count/
@@ -57,9 +45,18 @@ rm C9_5GR_1.count
 
 # delete outliers
 sed '/E2_6GP/d;/C9_5GR_1/d' sample_list > xso_exp_sample
-python bin/merge_count.py xso_exp_sample xso_exp_count.xls
+
+# featureCounts 
+#python bin/merge_count.py xso_exp_sample xso_exp_count.xls
+# stringtie
 python bin/merge_TPM.py xso_exp_sample xso_exp_TPM.xls
 
 
-#
-# awk '{print $1"\t"$1}' xso_exp_sample | sed 's/_[0-9]*$//' | sed '1,1isample\tcondition'> sample_design.txt
+###############
+# mapping rate
+###############
+cd /Pinus1/Liuhui/Xsorbifolium/gene_expression/ht_out
+grep -E "concordantly 1 time|Overall" *summary | \
+awk '{print $1,$NF}' | sed 's/(//;s/)//;s/.summary: /\t/' | \
+awk '{a[$1]=($1 in a ? a[$1]"\t"$2: $0)} END{for (k in a) print a[k]}' | \
+sed '1isamples\tunique_mappingRate\tmapping_rate' > ../Xso_RNAseq_MappingRate.xls
