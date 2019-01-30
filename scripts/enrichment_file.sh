bb94623020fc026bfd1d8e20d2bf19c778a706a8
Hui Liu <liuhui.bfu@gmail.com>
2019-01-30 11:27:12 +0100

    Create enrichment_file.sh

---
+++ scripts/enrichment_file.sh
@@ -0,0 +1,17 @@
+cd /mnt/crick/data/yellow_horn/Enrichment
+# pfam
+sed '1,1d' xso_Pfam.annot | awk '{print $2"\t"$1}' | \
+awk '{a[$1]=($1 in a ? a[$1]"|"$2: $0)} END{for (k in a) print a[k]}' | \
+sort -k1,1 > gene_to_pfam.tsv
+
+# go
+awk '{print $2"\t"$1}' xso_bg_GO.txt | \
+awk '{a[$1]=($1 in a ? a[$1]"|"$2: $0)} END{for (k in a) print a[k]}'| \
+sort -k 1,1 > gene_to_go.tsv
+
+# kegg
+sed '1,1d' kegg_annotation.xls | awk -F "\t" '{print $1"\t"$5}' | \
+awk '$2!="-"' | sed 's/EC://;s/ /|/g' | awk '!a[$0]++' | sort -k1,1 > gene_to_kegg.tsv
+
+#
+cp *tsv /mnt/crick/www/enrichment/yellowhorn
