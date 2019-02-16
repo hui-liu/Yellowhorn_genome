42ce4deb4ddf721c907905592f9f81f4439df62c
Hui Liu <liuhui.bfu@gmail.com>
2019-02-16 15:33:37 +0100

    Create backbone.sh

---
+++ Expression/backbone.sh
@@ -0,0 +1,32 @@
+#######
+# backbone
+#######
+# keep 1% of edges (0.99 2.326348)
+seidr backbone -o backbone/xso_irp_backbone_one_percent.sf -f 2.33 seidr_results/xso_irp_aggregated.sf 
+
+# keep 2% of edges (0.98 2.053749)
+seidr backbone -o backbone/xso_irp_backbone_two_percent.sf -f 2.05 seidr_results/xso_irp_aggregated.sf 
+
+# keep 3% of edges (0.97 1.880794)
+seidr backbone -o backbone/xso_irp_backbone_three_percent.sf -f 1.88 seidr_results/xso_irp_aggregated.sf 
+
+# keep 4% of edges (0.96 1.750686)
+seidr backbone -o backbone/xso_irp_backbone_four_percent.sf -f 1.75 seidr_results/xso_irp_aggregated.sf 
+
+# keep 5% of edges (0.95 1.644854)
+seidr backbone -o backbone/xso_irp_backbone_five_percent.sf -f 1.64 seidr_results/xso_irp_aggregated.sf 
+
+# keep 6% of edges (0.94 1.554774)
+seidr backbone -o backbone/xso_irp_backbone_six_percent.sf -f 1.55 seidr_results/xso_irp_aggregated.sf 
+
+# keep 7% of edges (0.93 1.475791)
+seidr backbone -o backbone/xso_irp_backbone_seven_percent.sf -f 1.48 seidr_results/xso_irp_aggregated.sf 
+
+# keep 8% of edges (0.92 1.405072)
+seidr backbone -o backbone/xso_irp_backbone_eight_percent.sf -f 1.4 seidr_results/xso_irp_aggregated.sf 
+
+# keep 9% of edges (0.91 1.340755)
+seidr backbone -o backbone/xso_irp_backbone_nine_percent.sf -f 1.34 seidr_results/xso_irp_aggregated.sf 
+
+# keep 10% of edges (0.9 1.281552)
+seidr backbone -o backbone/xso_irp_backbone_ten_percent.sf -f 1.28 seidr_results/xso_irp_aggregated.sf 
