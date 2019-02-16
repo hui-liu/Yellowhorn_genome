a0cdd919106a5a33aba6faea03217c797b60583b
Hui Liu <liuhui.bfu@gmail.com>
2019-02-16 15:39:04 +0100

    Create visualize_comparisons.sh

---
+++ Expression/visualize_comparisons.sh
@@ -0,0 +1,6 @@
+# plot and go enrichment
+cd /Pinus1/Liuhui/Xsorbifolium/gene_expression/seider
+for i in one two three four five six seven eight nine ten
+do
+Rscript bin/backbone_x_percent_threshold_compare.R compare/xso_irp_backbone_$i\_percent_threshold_compare.txt
+done
