6195f477a0ec831ed8cf4b3cf1f501098d457b80
Hui Liu <liuhui.bfu@gmail.com>
2019-02-20 17:26:57 +0100

    Update pfam_bed.py

--- Jbrowse/pfam_bed.py
+++ Jbrowse/pfam_bed.py
@@ -1,9 +1,9 @@
 import sys
 
 USAGE = "\nusage: python %s <gene.gff3> <interproscan.tsv> <pfam_bed.txt>\n" % sys.argv[0]
-#if len(sys.argv) != 3:
-#    print USAGE
-#    sys.exit()
+if len(sys.argv) != 4:
+    print USAGE
+    sys.exit()
 
 id_chr = {}
 with open(sys.argv[1], 'r') as f:
@@ -18,8 +18,8 @@ with open(sys.argv[2], 'r') as f:
     for line in f:
         lsp = line.split("\t")
         if lsp[3] == "Pfam":
-            id, start, end, desc = lsp[0], int(lsp[6]), int(lsp[7]), lsp[5]
+            id, start, end, pfam = lsp[0], int(lsp[6]), int(lsp[7]), lsp[4]
             chr = id_chr[id]
-            out.write("\t".join([chr, str(start-1), str(end), desc]) + "\n")
+            out.write("\t".join([chr, str(start-1), str(end), pfam]) + "\n")
 
 out.close()
