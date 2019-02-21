aa9635eaf36ea49b97c13986d361f625e2ee99ad
Hui Liu <liuhui.bfu@gmail.com>
2019-02-21 19:21:31 +0100

    Update configure.sh

--- Jbrowse/configure.sh
+++ Jbrowse/configure.sh
@@ -25,11 +25,11 @@ bin/flatfile-to-json.pl --gff /mnt/crick/data/yellow_horn/jbrowse/final.TE_jbrow
 --config '{"labelTranscripts": false, "category": "Annotation"}' --clientConfig '{ "textFont" : "normal 8px Univers,Helvetica,Arial,sans-serif"}'
 
 # (2.5) pfam domain
-cd /mnt/crick/data/yellow_horn
-python bin/pfam_bed.py annotation/final.protein_gene.gff3 functional_annotation/interproscan.tsv jbrowse/pfam_bed.txt
-cd /mnt/crick/www/yellowhorn/plugins/jbrowse
-bin/flatfile-to-json.pl --bed /mnt/crick/data/yellow_horn/jbrowse/pfam_bed.txt --trackLabel Pfam --trackType CanvasFeatures \
---config '{"labelTranscripts": false, "category": "Annotation"}' --clientConfig '{ "textFont" : "normal 8px Univers,Helvetica,Arial,sans-serif"}'
+#cd /mnt/crick/data/yellow_horn
+#python bin/pfam_bed.py annotation/final.protein_gene.gff3 functional_annotation/interproscan.tsv jbrowse/pfam_bed.txt
+#cd /mnt/crick/www/yellowhorn/plugins/jbrowse
+#bin/flatfile-to-json.pl --bed /mnt/crick/data/yellow_horn/jbrowse/pfam_bed.txt --trackLabel Pfam --trackType CanvasFeatures \
+#--config '{"labelTranscripts": false, "category": "Annotation"}' --clientConfig '{ "textFont" : "normal 8px Univers,Helvetica,Arial,sans-serif"}'
 
 # (3) SNPs
 cd data
