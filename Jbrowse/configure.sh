9b635b2058c8e11f9db761f7c9cec7f6efbb506e
Hui Liu <liuhui.bfu@gmail.com>
2019-02-26 10:41:28 +0100

    Update configure.sh

--- Jbrowse/configure.sh
+++ Jbrowse/configure.sh
@@ -50,8 +50,7 @@ bin/flatfile-to-json.pl --gff /mnt/crick/data/yellow_horn/jbrowse/promoter_up_1.
 --trackLabel "Promoter (upstream 1.5k)" --trackType CanvasFeatures --out data/yellowhorn \
 --config '{"labelTranscripts": false, "category": "Annotation"}' --clientConfig '{ "textFont" : "normal 8px Univers,Helvetica,Arial,sans-serif"}'
 
-# "color" : "#FF9F1C"
-
+# "color" : "#2EC4B6"
 
 # (3) SNPs
 cd data
